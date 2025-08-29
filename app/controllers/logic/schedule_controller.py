96d2bcb9e8ce0cce79b3c77d86e1291abe333980
Kirutre <gabrielpernalete3108@gmail.com>
2025-08-28 22:25:33 -0400

    improved controllers code

--- app/controllers/logic/schedule_controller.py
+++ app/controllers/logic/schedule_controller.py
@@ -21,9 +21,9 @@ class ScheduleController:
         day = self._sanitize(data)
         
         #! Modify the DB so that the respective fields are UNIQUE (eliminate this method if necessary)
-        self._validate(data, day)
+        validate_data = self._validate(data, day)
         
-        schedule = self._create_schedule_object(data, day)
+        schedule = self._create_schedule_object(validate_data)
         
         try:
             self._session.add(schedule)
@@ -37,60 +37,33 @@ class ScheduleController:
     def _sanitize(self, data: dict[str, any]) -> str:
         return data['day'].strip().lower().capitalize()
     
-    def _validate(self, data: dict[str, any], day: str) -> None:       
+    #* validates and converts data
+    def _validate(self, data: dict[str, any], day: str) -> dict[str, any]:       
         try:
-            datetime.strptime(data['start_time'], self.FORMAT)
-            datetime.strptime(data['end_time'], self.FORMAT)
+            start_time = datetime.strptime(data['start_time'], self.FORMAT).time()
+            end_time = datetime.strptime(data['end_time'], self.FORMAT).time()
             
             DayOfWeek(day)
             
         except ValueError as e:
             raise ValueError(f'Invalid input data: {e}')
         
-        if data['start_time'] > data['end_time']:
+        if start_time > end_time:
             raise ValueError('The start time cannot be greater than the end time')
 
         if self.exists(data, day):
             raise ObjectAlreadyExistsException(f'Schedule block already exists')
         
-    def _create_schedule_object(self, data: dict[str, any], day: str) -> Schedule:
+        return {'start_time': start_time, 'end_time': end_time, 'day': DayOfWeek(day)}
+        
+    def _create_schedule_object(self, data: dict[str, any]) -> Schedule:
         #* can be return self.model(**data)
         
         return Schedule(
             start_time = data['start_time'],
             end_time = data['end_time'],
-            day = DayOfWeek(day)
+            day = data['day']
         )
-        
-    def get_by_start_time(self, start_time: Time) -> Schedule:
-        statement = select(self._model).where(self._model.start_time == start_time)
-        
-        result = self._session.scalar(statement)
-        
-        if result is None:
-            raise ObjectNotFoundException(f'No Schedule found with start time "{start_time}"')
-        
-        return result
-        
-    def get_by_end_time(self, end_time: Time) -> Schedule:
-        statement = select(self._model).where(self._model.end_time == end_time)
-        
-        result = self._session.scalar(statement)
-        
-        if result is None:
-            raise ObjectNotFoundException(f'No Schedule found with end time "{end_time}"')
-        
-        return result
-        
-    def get_by_day(self, day: DayOfWeek) -> Schedule:        
-        statement = select(self._model).where(self._model.day == day)
-        
-        result = self._session.scalar(statement)
-        
-        if result is None:
-            raise ObjectNotFoundException(f'No Schedule found with "{day}" as the day')
-        
-        return result
     
     def get_all(self) -> list[Schedule]:
         return self._session.execute(select(self._model)).scalars().all()
